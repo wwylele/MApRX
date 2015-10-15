f8773a65ce58610840d4eb28956ff2b5b38ee9b3
wwylele <wwylele@gmail.com>
2015-10-15 09:58:41 +0300

    item table selection valid

--- src/MApRX_Qt/mainwindow.cpp
+++ src/MApRX_Qt/mainwindow.cpp
@@ -569,6 +569,7 @@ void MainWindow::on_buttonItemUp_clicked()
     if(!selection.isValid())return;
     u8 selItem=selection.row();
     if(selItem==0)return;
+    if(selItem>=map.metaData.itemCount)return;
     MoSwapItem mo(selItem-1);
     mo.toolTip=QString(tr("Move up Item#%1")).arg(selItem);
     doOperation(&mo);
@@ -583,6 +584,7 @@ void MainWindow::on_buttonItemDown_clicked()
     if(!selection.isValid())return;
     u8 selItem=selection.row();
     if(selItem==map.metaData.itemCount-1)return;
+    if(selItem>=map.metaData.itemCount)return;
     MoSwapItem mo(selItem);
     mo.toolTip=QString(tr("Move down Item#%1")).arg(selItem);
     doOperation(&mo);
@@ -596,6 +598,7 @@ void MainWindow::on_buttonItemDelete_clicked()
     QModelIndex selection=ui->itemTable->currentIndex();
     if(!selection.isValid())return;
     u8 selItem=selection.row();
+    if(selItem>=map.metaData.itemCount)return;
     MoDeleteItem mo(selItem);
     mo.toolTip=QString(tr("Remove Item#%1")).arg(selItem);
     doOperation(&mo);
